a9fe41bd2907d0f13609de56591e752e9c72c884
khs-kks <95934427+khs-kks@users.noreply.github.com>
2023-03-24 10:07:37 +0100

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,23 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "0888-415-990" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, phone: newPhone };
     let exists = false;
 
     persons.forEach((person) => {
@@ -22,6 +29,7 @@ const App = () => {
     if (!exists) {
       setPersons(persons.concat(newPerson));
       setNewName("");
+      setNewPhone("");
     } else {
       alert(`${newName} already exists in the book`);
     }
@@ -43,13 +51,24 @@ const App = () => {
           />
         </div>
         <div>
+          <label htmlFor="phone">Phone:</label>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            value={newPhone}
+            onChange={handlePhoneChange}
+            placeholder="Put your tel number here"
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name} {person.phone}</li>
         ))}
       </ul>
     </div>

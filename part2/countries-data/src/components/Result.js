626bbb0bdbcdc3c2d5661553a7547355016a983d
khs-kks <95934427+khs-kks@users.noreply.github.com>
2023-04-06 19:35:10 +0200

    2.20*: Data for countries, step3

--- part2/countries-data/src/components/Result.js
+++ part2/countries-data/src/components/Result.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function CountryListItem({ country }) {
   const [showDetails, setShowDetails] = useState(false);
@@ -36,6 +37,65 @@ function CountryListItem({ country }) {
 }
 
 const Result = ({ countries, searchTerm }) => {
+  const [weather, setWeather] = useState({});
+  const [weatherIcon, setWeatherIcon] = useState("");
+  const [weatherIconAltText, setWeatherIconAltText] = useState("");
+  const api_key = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    if (searchTerm !== "") {
+      const matchingCountries = countries.filter((country) =>
+        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+
+      if (matchingCountries.length === 1) {
+        const { name } = matchingCountries[0];
+        // const imgUrl = flags?.svg || "";
+
+        const fetchWeather = async () => {
+          const weatherInfo = await axios.get(
+            `https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${name.common}&aqi=no`
+          );
+
+          return weatherInfo.data.current;
+        };
+
+        const getWeather = async () => {
+          const weatherData = await fetchWeather();
+          
+          // const weatherIconUrl = weatherData.condition.icon.replace(
+          //   "//cdn.weatherapi.com",
+          //   "."
+          // );
+
+          const weatherIconUrl = weatherData.condition.icon;
+          console.log(weatherIconUrl);
+
+          setWeather(weatherData);
+          setWeatherIcon(weatherIconUrl);
+          setWeatherIconAltText(weatherData.condition.text);
+        };
+
+        getWeather();
+
+        return () => {
+          setWeather({});
+          setWeatherIcon("");
+          setWeatherIconAltText("");
+        };
+
+        // Ensure that `name` is defined before calling `fetchWeather` and `getWeather`
+      } else if (
+        matchingCountries.length === 0 ||
+        matchingCountries.length > 10
+      ) {
+        setWeather({});
+        setWeatherIcon("");
+        setWeatherIconAltText("");
+      }
+    }
+  }, [countries, searchTerm, api_key]);
+
   if (searchTerm === "") {
     return null;
   }
@@ -74,6 +134,10 @@ const Result = ({ countries, searchTerm }) => {
           ))}
         </ul>
         <img src={imgUrl} alt={`Logo of ${name.common}`} height="150" />
+        <h1>Weather in {capital || "the Capital"}</h1>
+        <p>Temperature in Celsius: {weather.temp_c}</p>
+        <img src={weatherIcon} alt={weatherIconAltText} height="100"></img>
+        <p>Wind in kph: {weather.wind_kph}</p>
       </div>
     );
   }
